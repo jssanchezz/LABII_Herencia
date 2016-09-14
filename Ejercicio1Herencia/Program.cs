563596284afd06f4ace5b063f832eb55e20e93e9
unknown <alumno@LAB02PC04.labs.extension>
2016-09-14 10:49:34 -0300

    Herencia

---
+++ Ejercicio1Herencia/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1Herencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Factura miFactura = new Factura();
+
+            List<Documento> milistado = new List<Documento>();
+
+            milistado.Add(miFactura);
+
+            Remito unRemito = new Remito();
+
+            milistado.Add(unRemito);
+
+            foreach (Documento item in milistado)
+            {
+                item.Mostrar();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio1Herencia/Remito.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1Herencia
+{
+    public class Remito:Documento
+    {
+        public string fecha;
+    }
+}
